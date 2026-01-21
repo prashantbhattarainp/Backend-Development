27f2ca8434d0e93e8230fe84236077c37c87a34d
Prashant Bhattarai <techandcode.prashant@gmail.com>
2026-01-21 23:45:06 +0530

    [Prashant] Added . unix commands

--- Question2/index.js
+++
@@ -1,7 +0,0 @@
-const utils = require("./string");
-
-const text = "hello world";
-
-console.log(utils.capitalize(text));
-console.log(utils.reverseString(text));
-console.log(utils.countVowels(text));
