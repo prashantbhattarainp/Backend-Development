a6a99a8cb9b842d6d49473d6d1688c30b792b7dd
Prashant Bhattarai <techandcode.prashant@gmail.com>
2026-02-02 11:45:18 +0530

    [Prashant]. Added classroom Practice

--- express-backend/index.js
+++ express-backend/index.js
@@ -4,10 +4,29 @@ const app = express();
 
 const PORT = 8000;
 
+const students =[
+    {id: 1, name: "Prashant", branch: "CSE"},
+    {id: 2, name: "Pacific", branch: "ECE"},
+    {id: 3, name: "Sohan", branch: "IT"}
+]
+
 app.get("/",(req,res)=>{
     res.send("Welcome to the home Page")
 })
 
+app.get("/students",(req,res)=>{
+    const branch = req.query.branch;
+    const foundStudents= students.filter(s=>s.branch==branch);
+    res.json(foundStudents)
+    // res.json(students)
+})
+
+app.get("/students/:id",(req,res)=>{
+    const id = parseInt(req.params.id)
+    const arrayIndex = students.findIndex(s=>s.id==id);
+    const data = students[arrayIndex];
+    res.json(data)
+})
 app.get("/user",(req,res)=>{
     res.send("<h1> This is User Page </h1>")
 })
